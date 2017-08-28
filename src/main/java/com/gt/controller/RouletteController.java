ecbd6ac0d1c71f7e142c694749e9167966613c76
Nitish <Nbansal@golden-technology.in>
2017-08-28 15:50:12 +0530

    GTROU 4 : RND complate

--- src/main/java/com/gt/controller/RouletteController.java
+++ src/main/java/com/gt/controller/RouletteController.java
@@ -1,21 +1,25 @@
 package com.gt.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.gt.beans.GameResponse;
+import com.gt.beans.Header;
 
+@EnableWebMvc
 @RestController
-@RequestMapping("/init")
 public class RouletteController {
 
-
- @RequestMapping(value = "/{name}", method = RequestMethod.GET)
- public @ResponseBody GameResponse getInit(@PathVariable String name) {
+//@RequestParam("initXml")
+ @RequestMapping(value= "/init",method = RequestMethod.POST, consumes= "application/xml")
+ public @ResponseBody GameResponse getInit(  @RequestBody(required = true) Header header) {
+	 System.out.println("1111111111111111");
  GameResponse game =new GameResponse();
  return game;
  }
+
 }
